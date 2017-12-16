6cb8e037f24d5555f177551fb95eb8f8b6a9e46b
Jon Bachelor <jon_bachelor@me.com>
2017-12-15 16:00:10 -0800

    Added basic network connectivity check on launch using connectivity abstraction.

--- NetStatus/App.xaml.cs
+++ NetStatus/App.xaml.cs
@@ -2,27 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Prism.Unity;
 using NetStatus.Views;
 using Xamarin.Forms;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
 
 namespace NetStatus
 {
     public partial class App : PrismApplication
     {
+        IConnectivity _connectivityService;
+
         public App(IPlatformInitializer initializer = null) : base(initializer) { }
 
         protected override void OnInitialized()
         {
             InitializeComponent();
 
-            NavigationService.NavigateAsync($"{nameof(NoNetworkPage)}");
+            var pageToNavigateTo = _connectivityService.IsConnected
+                ? nameof(NetworkViewPage)
+                : nameof(NoNetworkPage);
+
+            NavigationService.NavigateAsync($"{pageToNavigateTo}");
         }
 
         protected override void RegisterTypes()
         {
             Container.RegisterTypeForNavigation<NoNetworkPage>();
             Container.RegisterTypeForNavigation<NetworkViewPage>();
+
+            Container.RegisterInstance<IConnectivity>(CrossConnectivity.Current);
+            _connectivityService = Container.Resolve<IConnectivity>();
         }
     }
 }
